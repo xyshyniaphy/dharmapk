f02f78375eb60b17b184413df59b96b0e67bc27d
xyshyniaphy <xyshyniaphy@gmail.com>
2025-07-09 04:52:14 +0000

    adding xmind

--- src/App.tsx
+++ src/App.tsx
@@ -5,34 +5,14 @@ import './index.css'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center">
-      <div className="flex space-x-4 mb-8">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="h-24 w-24" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="h-24 w-24" alt="React logo" />
-        </a>
-      </div>
       <h1 className="text-3xl font-bold underline">    Hello world!  </h1>
-      <h1 className="text-5xl font-bold mb-4">Vite + React</h1>
       <div className="p-4 border border-gray-700 rounded-lg">
-        <button 
-          onClick={() => setCount((count) => count + 1)}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          count is {count}
-        </button>
-        <p className="mt-4">
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+       
       </div>
-      <p className="mt-8 text-gray-400">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </div>
   )
 }
